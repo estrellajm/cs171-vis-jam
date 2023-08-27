20bba4609cb40fe0379236473e09e8394213ebe6
Jose Estrella <estrellajosem@gmail.com>
2023-08-26 21:01:46 -0600

    checkpoint

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -10,10 +10,34 @@ import * as d3 from 'd3';
   styleUrls: ['./bar.component.scss'],
 })
 export class BarComponent {
+  dataset = [
+    25, 7, 5, 26, 11, 8, 25, 14, 23, 19, 14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+    24, 18, 25, 9, 3,
+  ];
   ngOnInit() {
+    this.genRandomDataSet(20);
+  }
+
+  genRandomDataSet(n: number) {
+    this.dataset = [...Array(n).keys()].map(() =>
+      Math.floor(Math.random() * 100)
+    );
     this.createD3();
   }
   createD3() {
-    d3.select('figure#d3').text('New Paragraph!');
+    d3.select('figure#wiring')
+      .selectAll('div')
+      .data(this.dataset)
+      .enter()
+      .append('div')
+      .attr('class', 'bar')
+      .style('height', (d) => `${d * 5}px`);
+    // .text((d) => d);
   }
+
+  processData = (d: any) => {
+    console.log(d);
+
+    return `Yesh! ${d}`;
+  };
 }
