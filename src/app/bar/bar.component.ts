54d2eb9bff8cead8d13e22e4677ce8b6dd98bdb0
Jose Estrella <estrellajosem@gmail.com>
2023-08-26 22:51:09 -0600

    completed updated barchart

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -14,53 +14,89 @@ export class BarComponent {
     5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23,
   ];
   ngOnInit() {
-    this.createD3();
+    this.createNewD3BarChart();
   }
 
-  createD3() {
+  createNewD3BarChart() {
+    const w = 500;
     const h = 100;
-    const w = 1000;
+    const barPadding = 1;
 
     const svg = d3
       .select('figure#wiring')
       .append('svg')
-      .attr('height', h)
-      .attr('width', w);
+      .attr('width', w)
+      .attr('height', h);
 
-    const circles = svg
-      .selectAll('circle')
+    svg
+      .selectAll('rect')
       .data(this.dataset)
       .enter()
-      .append('circle');
+      .append('rect')
+      .attr('x', (d, i) => i * (w / this.dataset.length))
+      .attr('y', (d) => h - d * 4)
+      .attr('width', w / this.dataset.length - barPadding)
+      .attr('height', (d) => d * 4)
+      .attr('fill', (d) => `rgb(0,0,${Math.round(d * 10)})`);
 
-    circles
-      .attr('cx', (d, i) => {
-        return i * 50 + 25;
-      })
-      .attr('cy', h / 2)
-      .attr('r', (d) => d)
-      .attr('fill', 'yellow')
-      .attr('stroke', 'orange')
-      .attr('stroke-width', (d) => d / 2);
-  }
-
-  createD3BarChart() {
-    d3.select('figure#wiring')
-      .selectAll('div')
+    svg
+      .selectAll('text')
       .data(this.dataset)
       .enter()
-      .append('div')
-      .attr('class', 'bar')
-      .style('height', (d) => `${d * 5}px`)
-      .text(this.processData);
+      .append('text')
+      .text((d) => d)
+      .attr('x', (d, i) => i * (w / this.dataset.length) + (w / this.dataset.length - barPadding) /2)
+      .attr('y', (d) => h - d * 4 + 14)
+      .attr('font-family','sans-serif')
+      .attr('font-size','11px')
+      .attr('fill','white')
+      .attr('text-anchor','middle')
   }
-  genRandomDataSet(n: number) {
-    this.dataset = [...Array(n).keys()].map(() =>
-      Math.floor(Math.random() * 100)
-    );
-    this.createD3BarChart();
-  }
-  processData = (d: any) => {
-    return `Yesh! ${d}`;
-  };
+
+  // createD3Circles() {
+  //   const h = 100;
+  //   const w = 1000;
+
+  //   const svg = d3
+  //     .select('figure#wiring')
+  //     .append('svg')
+  //     .attr('height', h)
+  //     .attr('width', w);
+
+  //   const circles = svg
+  //     .selectAll('circle')
+  //     .data(this.dataset)
+  //     .enter()
+  //     .append('circle');
+
+  //   circles
+  //     .attr('cx', (d, i) => {
+  //       return i * 50 + 25;
+  //     })
+  //     .attr('cy', h / 2)
+  //     .attr('r', (d) => d)
+  //     .attr('fill', 'yellow')
+  //     .attr('stroke', 'orange')
+  //     .attr('stroke-width', (d) => d / 2);
+  // }
+
+  // createD3BarChart() {
+  //   d3.select('figure#wiring')
+  //     .selectAll('div')
+  //     .data(this.dataset)
+  //     .enter()
+  //     .append('div')
+  //     .attr('class', 'bar')
+  //     .style('height', (d) => `${d * 5}px`)
+  //     .text(this.processData);
+  // }
+  // genRandomDataSet(n: number) {
+  //   this.dataset = [...Array(n).keys()].map(() =>
+  //     Math.floor(Math.random() * 100)
+  //   );
+  //   this.createD3BarChart();
+  // }
+  // processData = (d: any) => {
+  //   return `Yesh! ${d}`;
+  // };
 }
