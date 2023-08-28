49b72d59863bf2312eb900541535e49a168d5003
Jose Estrella <estrellajosem@gmail.com>
2023-08-28 00:10:02 -0600

    making progress

--- src/app/components/bar/bar.component.ts
+++ src/app/components/bar/bar.component.ts
@@ -10,17 +10,48 @@ import * as d3 from 'd3';
   styleUrls: ['./bar.component.scss'],
 })
 export class BarComponent {
-  dataset = [
-    5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23,
-  ];
   ngOnInit() {
-    this.createNewD3BarChart();
+    this.deScales();
   }
 
-  createNewD3BarChart() {
-    const w = 500;
-    const h = 100;
-    const barPadding = 1;
+  deScales() {
+    const dataset = [
+      [5, 20],
+      [480, 90],
+      [250, 50],
+      [100, 33],
+      [330, 95],
+      [410, 12],
+      [475, 44],
+      [25, 67],
+      [85, 21],
+      [220, 88],
+      [600, 150],
+    ];
+
+    const w = 1000;
+    const h = 300;
+    const padding = 20;
+
+    // const scale = (axis: 'x' | 'y') => {
+    //   if (axis === 'x') {
+    //     const max = d3.max(dataset, (d) => d[0])!;
+    //     return d3.scaleLinear().domain([0, max]).range([10, w]);
+    //   }
+    //   if (axis === 'y') {
+    //     const max = d3.max(dataset, (d) => d[1])!;
+    //     return d3.scaleLinear().domain([0, max]).range([10, h]);
+    //   }
+    //   return d3.scaleLinear().domain([0, 100]).range([10, h]);
+    // };
+    const xMax = d3.max(dataset, (d) => d[0])!;
+    const xScale = d3.scaleLinear().domain([0, xMax]).range([padding, w - padding * 2 - 3]);
+    const yMax = d3.max(dataset, (d) => d[1])!;
+    const yScale = d3.scaleLinear().domain([0, yMax]).range([h - padding, padding]);
+    const rMax = d3.max(dataset, (d) => d[1])!;
+    const rScale = d3.scaleLinear().domain([0, yMax]).range([2, 5]);
+    const aMax = d3.max(dataset, (d) => d[1])!;
+    const aScale = d3.scaleSqrt().domain([0, yMax]).range([0, 10]);
 
     const svg = d3
       .select('figure#wiring')
@@ -29,35 +60,119 @@ export class BarComponent {
       .attr('height', h);
 
     svg
-      .selectAll('rect')
-      .data(this.dataset)
+      .selectAll('circle')
+      .data(dataset)
       .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * (w / this.dataset.length))
-      .attr('y', (d) => h - d * 4)
-      .attr('width', w / this.dataset.length - barPadding)
-      .attr('height', (d) => d * 4)
-      .attr('fill', (d) => `rgb(0,0,${Math.round(d * 10)})`);
+      .append('circle')
+      .attr('cx', (d) => xScale(d[0]))
+      .attr('cy', (d) => yScale(d[1]))
+      .attr('r', (d) => aScale(d[1]));
 
     svg
       .selectAll('text')
-      .data(this.dataset)
+      .data(dataset)
       .enter()
       .append('text')
-      .text((d) => d)
-      .attr(
-        'x',
-        (d, i) =>
-          i * (w / this.dataset.length) +
-          (w / this.dataset.length - barPadding) / 2
-      )
-      // .attr('y', (d) => h - d * 4 + 14) // anchor text - top of bar
-      .attr('y', h - 5)
+      .text((d) => `${d[0]}, ${d[1]}`)
+
+      .attr('x', (d) => xScale(d[0]))
+      .attr('y', (d) => yScale(d[1]))
       .attr('font-family', 'sans-serif')
       .attr('font-size', '11px')
-      .attr('fill', 'white')
-      .attr('text-anchor', 'middle');
+      .attr('fill', 'red');
+
+
+      
   }
+
+  // d3ScatterPlot() {
+  //   const dataset = [
+  //     [5, 20],
+  //     [480, 90],
+  //     [250, 50],
+  //     [100, 33],
+  //     [330, 95],
+  //     [410, 12],
+  //     [475, 44],
+  //     [25, 67],
+  //     [85, 21],
+  //     [220, 88],
+  //   ];
+
+  //   const w = 500;
+  //   const h = 100;
+
+  //   const svg = d3
+  //     .select('figure#wiring')
+  //     .append('svg')
+  //     .attr('width', w)
+  //     .attr('height', h);
+
+  //   svg
+  //     .selectAll('circle')
+  //     .data(dataset)
+  //     .enter()
+  //     .append('circle')
+  //     .attr('cx', (d) => d[0])
+  //     .attr('cy', (d) => d[1])
+  //     .attr('r', (d) => Math.sqrt(h - d[1]));
+
+  //   svg
+  //     .selectAll('text')
+  //     .data(dataset)
+  //     .enter()
+  //     .append('text')
+  //     .text((d) => `${d[0]}, ${d[1]}`)
+  //     .attr('x', (d) => d[0])
+  //     .attr('y', (d) => d[1])
+  //     .attr('font-family', 'sans-serif')
+  //     .attr('font-size', '11px')
+  //     .attr('fill', 'red');
+  // }
+
+  // createNewD3BarChart() {
+  //   const w = 500;
+  //   const h = 100;
+  //   const barPadding = 1;
+
+  //   const dataset = [
+  //     5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23,
+  //   ];
+  //   const svg = d3
+  //     .select('figure#wiring')
+  //     .append('svg')
+  //     .attr('width', w)
+  //     .attr('height', h);
+
+  //   svg
+  //     .selectAll('rect')
+  //     .data(dataset)
+  //     .enter()
+  //     .append('rect')
+  //     .attr('x', (d, i) => i * (w / dataset.length))
+  //     .attr('y', (d) => h - d * 4)
+  //     .attr('width', w / dataset.length - barPadding)
+  //     .attr('height', (d) => d * 4)
+  //     .attr('fill', (d) => `rgb(0,0,${Math.round(d * 10)})`);
+
+  //   svg
+  //     .selectAll('text')
+  //     .data(dataset)
+  //     .enter()
+  //     .append('text')
+  //     .text((d) => d)
+  //     .attr(
+  //       'x',
+  //       (d, i) =>
+  //         i * (w / dataset.length) + (w / dataset.length - barPadding) / 2
+  //     )
+  //     // .attr('y', (d) => h - d * 4 + 14) // anchor text - top of bar
+  //     .attr('y', h - 5)
+  //     .attr('font-family', 'sans-serif')
+  //     .attr('font-size', '11px')
+  //     .attr('fill', 'white')
+  //     .attr('text-anchor', 'middle');
+  // }
 
   // createD3Circles() {
   //   const h = 100;
