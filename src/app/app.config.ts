4a6d3092d4565c51f94e3fb279a56d8c1d626abe
Jose Estrella <estrellajosem@gmail.com>
2023-11-18 02:39:55 -0700

    pretty

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,21 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"cs171-vis-jam","appId":"1:311393750868:web:e46baba56c4a5136b342c1","storageBucket":"cs171-vis-jam.appspot.com","apiKey":"AIzaSyCJRzZ7u74dvAkXcP8MJ_geLnIm6LKo6G4","authDomain":"cs171-vis-jam.firebaseapp.com","messagingSenderId":"311393750868"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'cs171-vis-jam',
+          appId: '1:311393750868:web:e46baba56c4a5136b342c1',
+          storageBucket: 'cs171-vis-jam.appspot.com',
+          apiKey: 'AIzaSyCJRzZ7u74dvAkXcP8MJ_geLnIm6LKo6G4',
+          authDomain: 'cs171-vis-jam.firebaseapp.com',
+          messagingSenderId: '311393750868',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
