28a8f5adb63868e8a73831177f602ab569a8b0bc
Jose Estrella <estrellajosem@gmail.com>
2023-11-20 00:30:26 -0700

    added new earth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BarComponent } from './components/bar/bar.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { EarthComponent } from './components/earth/earth.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     BrowserModule,
     AppRoutingModule,
     BarComponent,
+    EarthComponent,
     provideFirebaseApp(() => initializeApp({"projectId":"cs171-vis-jam","appId":"1:311393750868:web:e46baba56c4a5136b342c1","storageBucket":"cs171-vis-jam.appspot.com","apiKey":"AIzaSyCJRzZ7u74dvAkXcP8MJ_geLnIm6LKo6G4","authDomain":"cs171-vis-jam.firebaseapp.com","messagingSenderId":"311393750868"})),
     provideFirestore(() => getFirestore()),
   ],
