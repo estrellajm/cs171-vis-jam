03d85ba7cd72f9b2dca1e240a0fab67865a0eef5
Jose Estrella <estrellajosem@gmail.com>
2023-11-21 14:23:56 -0700

    setup standalone lazy loading component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./main/pages/welcome/welcome.page').then((m) => m.WelcomePage),
+  },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }, // redirect to `welcome`
+  { path: '**', redirectTo: '/welcome' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
