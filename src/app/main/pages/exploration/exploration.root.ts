1fd1da89b6a5000cef79d0c7b871693ccc722840
Jose Estrella <estrellajosem@gmail.com>
2023-11-22 22:20:34 -0700

    added tool tip

--- src/app/main/pages/exploration/exploration.root.ts
+++ src/app/main/pages/exploration/exploration.root.ts
@@ -1,6 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: 'exploration.root.html',
+  templateUrl: './exploration.root.html',
 })
-export class ExplorationPage {}
+export class ExplorationPage {
+  routes = [
+    {
+      route: 'economy',
+      title: 'Economy',
+    },
+    {
+      route: 'education',
+      title: 'Education',
+    },
+    {
+      route: 'environment',
+      title: 'Environment',
+    },
+    {
+      route: 'correlation',
+      title: 'Correlation',
+    },
+  ];
+  isTooltipVisible = false;
+  tooltipContent = '';
+  tooltipStyle = {};
+
+  showTooltip(event: MouseEvent, content: string) {
+    this.tooltipContent = content;
+    this.tooltipStyle = {
+      left: `${event.x + 20}px`,
+      top: `${event.y - 25}px`, // Adjust as needed for positioning
+    };
+    this.isTooltipVisible = true;
+  }
+
+  hideTooltip() {
+    this.isTooltipVisible = false;
+    this.tooltipStyle = {
+      left: `0px`,
+      top: `0px`, // Adjust as needed for positioning
+    };
+  }
+}
