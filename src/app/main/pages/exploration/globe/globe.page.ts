681ebbd9d40891d3008ae3b9a627e62d0b713a93
Jose Estrella <estrellajosem@gmail.com>
2023-11-23 00:33:09 -0700

    routing is done

--- src/app/main/pages/exploration/globe/globe.page.ts
+++ src/app/main/pages/exploration/globe/globe.page.ts
@@ -1,7 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Country } from '@interfaces/country.interface';
+import { Observable, map } from 'rxjs';
 
+type RouteKey = 'economy' | 'education' | 'environment';
 @Component({
   selector: 'app-globe',
   standalone: true,
@@ -10,15 +13,39 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
   styleUrls: ['./globe.page.scss'],
 })
 export class GlobePage {
-  data: any;
+  data: Country[];
+  title: string;
+  path$: Observable<{ previous: string; next: string }>;
+
+  routes = {
+    economy: {
+      previous: 'welcome',
+      next: 'education',
+    },
+    education: {
+      previous: 'economy',
+      next: 'environment',
+    },
+    environment: {
+      previous: 'education',
+      next: 'correlation',
+    },
+  };
 
   route = inject(ActivatedRoute);
 
   ngOnInit() {
     this.route.data.subscribe((data) => {
-      console.log(data['data'][0]);
-      
+      this.path$ = this.route.data.pipe(
+        map((data: any) => this.routes[data['path'] as RouteKey])
+      );
+      this.title = data['path'];
       this.data = data['data'];
     });
+  }
+
+  getPreviousPath(previous: string): string {
+    if (previous === 'welcome') return 'welcome';
+    return `exploration/${previous}`;
   }
 }
