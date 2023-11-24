f756f2b8d03b8aaaf532bd9643d766dfb96e84ce
Jose Estrella <estrellajosem@gmail.com>
2023-11-24 03:27:24 -0700

    updated correlation

--- src/app/main/pages/exploration/correlation/correlation.page.ts
+++ src/app/main/pages/exploration/correlation/correlation.page.ts
@@ -9,4 +9,38 @@ import { RouterModule } from '@angular/router';
   templateUrl: './correlation.page.html',
   styleUrls: ['./correlation.page.scss'],
 })
-export class CorrelationPage {}
+export class CorrelationPage {
+  selectedX: string = 'GDP Per Capita';
+  selectedY: string = 'Literacy Rate';
+  selectedCountry: string = 'Germany';
+  selectedYear: number = 2018;
+
+  showXDropdown: boolean = false;
+  showYDropdown: boolean = false;
+  showCountryDropdown: boolean = false;
+  showYearDropdown: boolean = false;
+
+  axisValues: string[] = ['GDP Per Capita', 'Literacy Rate'];
+  countries: string[] = ['Germany', 'US'];
+  years: number[] = [2018, 2019];
+
+  changeSelectedX(newX: string) {
+    this.selectedX = newX;
+    this.showXDropdown = false;
+  }
+
+  changeSelectedY(newY: string) {
+    this.selectedY = newY;
+    this.showYDropdown = false;
+  }
+
+  changeSelectedCountry(newCountry: string) {
+    this.selectedCountry = newCountry;
+    this.showCountryDropdown = false;
+  }
+
+  changeSelectedYear(newYear: number) {
+    this.selectedYear = newYear;
+    this.showYearDropdown = false;
+  }
+}
