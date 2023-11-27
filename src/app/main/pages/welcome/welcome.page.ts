64b2c3bec7c40a39e5e8e808f69fedffb2e48fc7
Jose Estrella <estrellajosem@gmail.com>
2023-11-26 23:08:25 -0700

    added random stars

--- src/app/main/pages/welcome/welcome.page.ts
+++ src/app/main/pages/welcome/welcome.page.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,4 +9,24 @@ import { RouterModule } from '@angular/router';
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
 })
-export class WelcomePage {}
+export class WelcomePage {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.addStars();
+  }
+
+  private addStars() {
+    const container = this.el.nativeElement.querySelector(
+      '.h-screen.bg-black.bg-globe-pattern'
+    );
+    for (let i = 0; i < 500; i++) {
+      // Number of stars
+      const star = this.renderer.createElement('div');
+      this.renderer.addClass(star, 'star');
+      this.renderer.setStyle(star, 'left', `${Math.random() * 100}%`);
+      this.renderer.setStyle(star, 'top', `${Math.random() * 100}%`);
+      this.renderer.appendChild(container, star);
+    }
+  }
+}
