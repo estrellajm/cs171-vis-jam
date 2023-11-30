78e940991cc69d03c9dda1c72eded60d4434bde7
Jose Estrella <estrellajosem@gmail.com>
2023-11-30 06:22:31 -0700

    added csv file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,17 +14,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'cs171-vis-jam',
-        appId: '1:311393750868:web:e46baba56c4a5136b342c1',
-        storageBucket: 'cs171-vis-jam.appspot.com',
-        apiKey: 'AIzaSyCJRzZ7u74dvAkXcP8MJ_geLnIm6LKo6G4',
-        authDomain: 'cs171-vis-jam.firebaseapp.com',
-        messagingSenderId: '311393750868',
-      })
-    ),
-    provideFirestore(() => getFirestore()),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/core/services/data/data.service.ts
+++ src/app/core/services/data/data.service.ts
@@ -1,18 +1,46 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { Country } from '@interfaces/country.interface';
-import { WD_INDICATORS } from '@assets/wd_indicators'; // Adjust the path as needed
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  wd_indicators = WD_INDICATORS;
-  getData(path: string): Country[] {
-    const countries = this.wd_indicators.map((item: any) => ({
-      country: item.country,
-      code: item.code,
-      [path]: item[path],
-    }));
-    return countries;
+  private http = inject(HttpClient);
+
+  public getCsvData(): Observable<any[]> {
+    const csvUrl = 'assets/data/wd_indicators.csv';
+    return this.http
+      .get(csvUrl, { responseType: 'text' })
+      .pipe(map(this.extractData));
+  }
+
+  private extractData(csv: string): any[] {
+    const lines = csv.split('\n');
+    return lines.map((line) => line.split(','));
+  }
+
+  getData(path: string): Observable<Country[]> {
+    return this.getCsvData().pipe(
+      map((csvData) => {
+        // Assuming the first row of CSV data contains headers
+        const headers = csvData[0];
+        const dataRows = csvData.slice(1);
+
+        return dataRows.map((row) => {
+          const item = row.reduce((obj: any, val: any, index: any) => {
+            obj[headers[index]] = val;
+            return obj;
+          }, {} as any);
+
+          return {
+            country: item.country,
+            code: item.code,
+            [path]: item[path],
+          } as Country;
+        });
+      })
+    );
   }
 }
