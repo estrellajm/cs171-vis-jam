8ed6dfbbc0dfaf856f2057930a663e611b33b98d
Jose Estrella <estrellajosem@gmail.com>
2023-12-10 01:37:00 -0700

    minor cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, VERSION, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { slider } from './slider';
 import { Store } from '@ngxs/store';
 import { LoadCountries } from './core/stores/countries/countries.actions';
+import { slider } from './slider';
 
 @Component({
   selector: 'jam-root',
@@ -19,30 +18,10 @@ import { LoadCountries } from './core/stores/countries/countries.actions';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-
-  routes = [
-    {
-      route: 'welcome',
-      title: 'Welcome',
-    },
-    {
-      route: 'exploration',
-      title: 'Exploration',
-    },
-    {
-      route: 'correlation',
-      title: 'Correlation',
-    },
-    {
-      route: 'credits',
-      title: 'Credits',
-    },
-  ];
-
   store = inject(Store);
   constructor() {
     this.store.dispatch(LoadCountries);
+    console.log(`Angular ${VERSION.full}`);
   }
 
   prepareRoute(outlet: RouterOutlet) {
